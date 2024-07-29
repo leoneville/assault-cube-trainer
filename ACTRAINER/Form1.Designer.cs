08102339652ebc05a747ccf1ebc14e156de0d015
leoneville <leoneville_@hotmail.com>
2024-07-29 16:52:01 -0300

    first commit

---
+++ ACTRAINER/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ACTRAINER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Attach";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(145, 52);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 24);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Ammo";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(145, 94);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 24);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Health";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(368, 199);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Assault Cube Hack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ ACTRAINER/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Memory;
+
+namespace ACTRAINER
+{
+    public partial class Form1 : Form
+    {
+        Mem memory = new Mem();
+        public static string rAmmo = "ac_client.exe+0x0017E0A8,140";
+        public static string health = "ac_client.exe+0x0017E0A8,EC";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread rAmmo = new Thread(F);
+            rAmmo.Start();
+
+            Thread health = new Thread(H);
+            health.Start();
+        }
+
+        private void F()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    memory.WriteMemory(rAmmo, "int", "100");
+                    Thread.Sleep(5);
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private void H()
+        {
+            while (true)
+            {
+                if (checkBox2.Checked)
+                {
+                    memory.WriteMemory(health, "int", "1000");
+                    Thread.Sleep(5);
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int processID = memory.GetProcIdFromName("ac_client");
+            if (processID > 0)
+            {
+                memory.OpenProcess(processID);
+                MessageBox.Show($"Found process, {processID}"); 
+            }
+            else
+            {
+                MessageBox.Show($"Process not found");
+            }
+
+        }
+    }
+}
